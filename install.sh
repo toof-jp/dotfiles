83cb553e28ae97be95f6dff3653c344247a3b215
toof-jp <toof.jp.mail@gmail.com>
2021-02-24 12:18:22 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 ln -sf ${PWD}/.zshrc ~/.zshrc
 ln -sf ${PWD}/.tmux.conf ~/.tmux.conf
-ln -sf ${PWD}/.config ~/.config
+mkdir -p ~/.config
+ln -sf ${PWD}/.config/git ~/.config/git
+ln -sf ${PWD}/.config/nvim ~/.config/nvim
