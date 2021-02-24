441cd306e0bd73dd7e13f49a7478c310018e8f9f
toof-jp <toof.jp.mail@gmail.com>
2021-02-24 13:55:23 +0900

    Update tmux config

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 ln -sf ${PWD}/.zshrc ~/.zshrc
-ln -sf ${PWD}/.tmux.conf ~/.tmux.conf
 mkdir -p ~/.config
 ln -sf ${PWD}/.config/git ~/.config/git
 ln -sf ${PWD}/.config/nvim ~/.config/nvim
+ln -sf ${PWD}/.config/tmux ~/.config/tmux
