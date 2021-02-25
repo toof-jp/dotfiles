491855c5ba0e19d91d73c3d958f8a0e5174c35c4
toof-jp <toof.jp.mail@gmail.com>
2021-02-25 17:46:15 +0900

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 ln -sf ${PWD}/.zshrc ~/.zshrc
 mkdir -p ~/.config
-ln -sf ${PWD}/.config/git ~/.config/git
-ln -sf ${PWD}/.config/nvim ~/.config/nvim
-ln -sf ${PWD}/.config/tmux ~/.config/tmux
+ln -sf ${PWD}/.config/git ~/.config
+ln -sf ${PWD}/.config/nvim ~/.config
+ln -sf ${PWD}/.config/tmux ~/.config
