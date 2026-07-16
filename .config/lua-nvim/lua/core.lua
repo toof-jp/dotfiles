7aaf9bf139864dc3d6f7da66b1e0f10311e72d75
Fumiya Tokumasu <toof@toof.jp>
2026-07-16 23:11:48 +0900

    feat: update neovim config

--- .config/lua-nvim/lua/core.lua
+++ .config/lua-nvim/lua/core.lua
@@ -8,5 +8,10 @@ vim.opt.shiftwidth = 2 -- use two spaces for auto indent
 
 vim.keymap.set("x", "p", '"_dP', { desc = "paste in visual mode without yanking selection" })
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { silent = true, desc = "exit terminal-mode with Esc" })
+vim.keymap.set("n", "Q", function()
+  local dir = vim.fn.expand("%:p:h")
+  vim.cmd("bd")
+  require("oil").open(dir)
+end, { desc = "delete current buffer and return to oil" })
 
 vim.api.nvim_create_user_command("Q", "q!", {})
--- .config/lua-nvim/lua/plugins.lua
+++ .config/lua-nvim/lua/plugins.lua
@@ -16,6 +16,7 @@ return {
           show_hidden = true,
         }
       })
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     end,
   },
   {
